b80f7d32574c353219bd005d26f8d8de5977d952
anacosantos <anacarolinaosantos85@gmail.com>
2021-02-22 12:06:38 +0200

    Add allure-report

--- utils/configCreator.js
+++ utils/configCreator.js
@@ -4,6 +4,10 @@ module.exports = provideConfig => {
     specs: ['../specs/*.spec.js'],
     onPrepare: () => {
       browser.waitForAngularEnabled(false)
+      var AllureReporter = require('jasmine-allure-reporter');
+      jasmine.getEnv().addReporter(new AllureReporter({
+        resultsDir: 'allure-results'
+      }));
     },
     jasmineNodeOpts: { random: true },
     directConnect: true
