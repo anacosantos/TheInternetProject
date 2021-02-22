b40fa6ab0348515b074cdb78e4908b17cac440ba
anacosantos <anacarolinaosantos85@gmail.com>
2021-02-22 18:15:14 +0200

    fix lint

--- page-objects/login.js
+++ page-objects/login.js
@@ -1,15 +1,15 @@
 const faker = require('faker')
 class Login {
-    constructor(){
-        this.userNameLabel = element(by.xpath("//label[.='Username']"))
-        this.userNameField = faker.name.findName();
-        this.passwordLabel = element(by.xpath("//label[.='Password']"))
-        this.passwordField = element(by.xpath("//input[@id='password']"))
-        this.login = element(by.xpath("//i[@class='fa fa-2x fa-sign-in']"))
-    }
+  constructor () {
+    this.userNameLabel = element(by.xpath("//label[.='Username']"))
+    this.userNameField = faker.name.findName()
+    this.passwordLabel = element(by.xpath("//label[.='Password']"))
+    this.passwordField = element(by.xpath("//input[@id='password']"))
+    this.login = element(by.xpath("//i[@class='fa fa-2x fa-sign-in']"))
+  }
 
-    visit(){
-        browser.get("/login")
-    }
+  visit () {
+    browser.get('/login')
+  }
 }
 module.exports = Login
--- specs/login.spec.js
+++ specs/login.spec.js
@@ -1,21 +1,16 @@
 const Login = require('../page-objects/login')
-const faker = require('faker')
 
 describe('Given the login page', () => {
-    let loginpage
-  
-    beforeAll(() => {
-      loginpage = new Login()
-      loginpage.visit()
-    })
-    describe('Testing loginpage,', () => {
-      it('Check if userNameLabel and passwordLabel shows up', () => {
-          
-        expect(loginpage.userNameLabel.getText()).toBe('Username')
-        expect(loginpage.passwordLabel.getText()).toBe('Password')
+  let loginpage
 
-      })
-      
+  beforeAll(() => {
+    loginpage = new Login()
+    loginpage.visit()
+  })
+  describe('Testing loginpage,', () => {
+    it('Check if userNameLabel and passwordLabel shows up', () => {
+      expect(loginpage.userNameLabel.getText()).toBe('Username')
+      expect(loginpage.passwordLabel.getText()).toBe('Password')
     })
   })
-  
+})
