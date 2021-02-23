98c615f6d1af4ef867834a477a5d3c48d2767721
anacosantos <anacarolinaosantos85@gmail.com>
2021-02-23 18:31:11 +0200

    Login page test

--- page-objects/login.js
+++ page-objects/login.js
@@ -1,11 +1,14 @@
 const faker = require('faker')
+const { element } = require('protractor')
 class Login {
   constructor () {
     this.userNameLabel = element(by.xpath("//label[.='Username']"))
-    this.userNameField = faker.name.findName()
     this.passwordLabel = element(by.xpath("//label[.='Password']"))
+    this.userNameField = element(by.xpath("//input[@id='username']"))
     this.passwordField = element(by.xpath("//input[@id='password']"))
-    this.login = element(by.xpath("//i[@class='fa fa-2x fa-sign-in']"))
+    this.logIn = element(by.css(".fa"))
+    //his.browserSecure = browser.get('/secure')
+   // this.successMessage = element(by.class('.flash sucess'))
   }
 
   visit () {
@@ -13,3 +16,4 @@ class Login {
   }
 }
 module.exports = Login
+
--- specs/login.spec.js
+++ specs/login.spec.js
@@ -12,5 +12,12 @@ describe('Given the login page', () => {
       expect(loginpage.userNameLabel.getText()).toBe('Username')
       expect(loginpage.passwordLabel.getText()).toBe('Password')
     })
+    it('Check corret username and password', () => {
+      expect(loginpage.userNameField.sendKeys('tomsmith'))
+      expect(loginpage.passwordField.sendKeys('SuperSecretPassword!'))
+      expect(loginpage.logIn.click())
+      //expect(loginpage.browserSecure)
+      //expect(loginpage.browserSecure.sucessMessage)
+    })
   })
 })
