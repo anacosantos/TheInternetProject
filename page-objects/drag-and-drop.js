a3e8bce113b3b56266e743849bb0ba825bd0d394
anacosantos <anacarolinaosantos85@gmail.com>
2021-03-25 11:11:08 +0200

    Drag and drop modifications

--- page-objects/drag-and-drop.js
+++ page-objects/drag-and-drop.js
@@ -1,8 +1,12 @@
 const { browser } = require('protractor')
 
 class DragAndDrop {
+  titleA = 'A'
+  titleB = 'B'
+  dragAndDropTitle = 'Drag and Drop'
+
   constructor () {
-    this.title = element(by.xpath("//h3[.='Drag and Drop']"))
+    this.title = element(by.xpath("//h3[.='" + this.dragAndDropTitle + "']"))
     this.boxesId = element.all(by.id('columns'))
     this.boxA = element(by.id('column-a'))
     this.boxB = element(by.id('column-b'))
--- specs/drag-and-drop/drag_and_drop.spec.js
+++ specs/drag-and-drop/drag_and_drop.spec.js
@@ -10,18 +10,18 @@ describe('Give me the Drag and Drop page', () => {
   })
 
   it('Check if Drag and Drop title exists', () => {
-    expect(dragDrop.title.getText()).toBe('Drag and Drop')
+    expect(dragDrop.title.getText()).toBe(dragDrop.dragAndDropTitle)
   })
 
   it('Check if boxes A and B exist', () => {
-    expect(dragDrop.boxA.getText()).toBe('A')
-    expect(dragDrop.boxB.getText()).toBe('B')
+    expect(dragDrop.boxA.getText()).toBe(dragDrop.titleA)
+    expect(dragDrop.boxB.getText()).toBe(dragDrop.titleB)
   })
 
   it('Check if box A change to box B', async () => {
     browser.executeScript(dnd, dragDrop.boxA.getWebElement(), dragDrop.boxB.getWebElement())
     browser.sleep(5000)
-    expect(dragDrop.boxA.getText()).toBe('B')
-    expect(dragDrop.boxB.getText()).toBe('A')
+    expect(dragDrop.boxA.getText()).toBe(dragDrop.titleB)
+    expect(dragDrop.boxB.getText()).toBe(dragDrop.titleA)
   })
 })
