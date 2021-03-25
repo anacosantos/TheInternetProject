da62d714b2482bc339b550f23a625d47df277455
anacosantos <anacarolinaosantos85@gmail.com>
2021-03-25 11:40:56 +0200

    drag and drop modif

--- page-objects/drag-and-drop.js
+++ page-objects/drag-and-drop.js
@@ -1,19 +1,27 @@
 const { browser } = require('protractor')
 
 class DragAndDrop {
-  titleA = 'A'
-  titleB = 'B'
-  dragAndDropTitle = 'Drag and Drop'
-
   constructor () {
-    this.title = element(by.xpath("//h3[.='" + this.dragAndDropTitle + "']"))
+    this.title = element(by.xpath("//h3[.='" + this.getDragAndDropTitle() + "']"))
     this.boxesId = element.all(by.id('columns'))
     this.boxA = element(by.id('column-a'))
     this.boxB = element(by.id('column-b'))
   }
 
-  visit () {
+  visit() {
     browser.get('drag_and_drop')
+  }
+
+  getTitleA() {
+    return 'A'
+  }
+
+  getTitleB() {
+    return 'B'
+  }
+
+  getDragAndDropTitle() {
+    return 'Drag and Drop'
   }
 }
 
--- specs/drag-and-drop/drag_and_drop.spec.js
+++ specs/drag-and-drop/drag_and_drop.spec.js
@@ -10,18 +10,18 @@ describe('Give me the Drag and Drop page', () => {
   })
 
   it('Check if Drag and Drop title exists', () => {
-    expect(dragDrop.title.getText()).toBe(dragDrop.dragAndDropTitle)
+    expect(dragDrop.title.getText()).toBe(dragDrop.getDragAndDropTitle())
   })
 
   it('Check if boxes A and B exist', () => {
-    expect(dragDrop.boxA.getText()).toBe(dragDrop.titleA)
-    expect(dragDrop.boxB.getText()).toBe(dragDrop.titleB)
+    expect(dragDrop.boxA.getText()).toBe(dragDrop.getTitleA())
+    expect(dragDrop.boxB.getText()).toBe(dragDrop.getTitleB())
   })
 
-  it('Check if box A change to box B', async () => {
+  it('Check if box A change to box B', () => {
+    // I used dnd beacuse the protractor way doesn't work
     browser.executeScript(dnd, dragDrop.boxA.getWebElement(), dragDrop.boxB.getWebElement())
-    browser.sleep(5000)
-    expect(dragDrop.boxA.getText()).toBe(dragDrop.titleB)
-    expect(dragDrop.boxB.getText()).toBe(dragDrop.titleA)
+    expect(dragDrop.boxA.getText()).toBe(dragDrop.getTitleB())
+    expect(dragDrop.boxB.getText()).toBe(dragDrop.getTitleA())
   })
 })
