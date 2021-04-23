f309cdfe42bff64a7c4ff33d00f526b9e7701d18
anacosantos <anacarolinaosantos85@gmail.com>
2021-04-23 19:17:34 +0300

    uplaod almost finished , last test

--- page-objects/file-uploader.js
+++ page-objects/file-uploader.js
@@ -7,6 +7,8 @@ class FileUploader {
         this.uploadButton = element(by.css('#file-submit'))
         this.fileUploadedMessage = element(by.css('.example h3'))
         this.dragAndDropId = element(by.id('drag-drop-upload'))
+        //this.sucessMark = element(by.css('.dz-.dz-preview dz-processing dz-image-preview dz-success dz-complete-mark'))
+        this.sucessMark = element(by.xpath("//div[@class='dz-preview dz-processing dz-image-preview dz-success dz-complete']//span[.='✔']"))
     }
     
     visit (){
--- specs/file-uploader/file-uploader.spec.js
+++ specs/file-uploader/file-uploader.spec.js
@@ -1,7 +1,43 @@
-const dropFile = require("../me.jpg");
-const FileUploader = require('../../page-objects/file-uploader')
+
+const FileUploader = require('../../page-objects/file-uploader');
 var path = require('path');
 const { browser, ExpectedConditions } = require('protractor');
+//var filePath = require('/Users/anacarolinaoliveiradossantos/Documents/TheInternetProject/uploads/me.jpg');
+//var filePath = require('../../uploads/me.jpg');
+var filePath = path.resolve(__dirname, '/Users/anacarolinaoliveiradossantos/Documents/TheInternetProject/uploads/me.jpg')
+const fs = require('fs')
+const dnd = require('html-dnd').code
+const dropFile = require("../../utils/drop-file");
+
+
+// const dropFile = function (dropArea, filePath)  {
+//     // get the full path
+//     filePath = path.resolve(filePath);
+//     // console.log(filePath)
+  
+//      // assert the file is present
+//     fs.accessSync(filePath, fs.F_OK);
+  
+//     // resolve the drop area
+
+//     return fileUploader.dragAndDropId.getWebElement().then(function (element) {
+//         browser.executeScript(fileUploader.dragAndDropId, element).then(function (input) {
+  
+              
+//                 input.sendKeys(filePath);
+//               });
+//     // return dropArea.getWebElement().then(function (element) {
+//     //     browser.wait(ExpectedConditions.presenceOf(element))
+//     //     fileUploader.uploadButton.click()
+//     //     expect(fileUploader.dragAndDropId.getText()).toBe('File Uploade!')
+//     //   // bind a new input to the drop area
+//     // //   browser.executeScript(JS_BIND_INPUT, element).then(function (input) {
+  
+//     //     // upload the file to the new input
+//     //     input.sendKeys(filePath);
+//       //});
+//     });
+// };
 
 describe('Give me File Uploader page', () => {
     const fileUploader = new FileUploader()
@@ -26,12 +62,45 @@ describe('Give me File Uploader page', () => {
     // })
 
     it('Check if drag and drop a file into the area works', () => {
-        dropFile(fileUploader.dragAndDropId, '../me.jpg');
-        browser.sleep(5000)
-        // wait for the droped image to be displayed in the drop area
-        browser.wait(ExpectedConditions.presenceOf(fileUploader.dragAndDropId))
+        dropFile(fileUploader.dragAndDropId, filePath)
+        //browser.get('https://the-internet.herokuapp.com/upload');
+        browser.wait(ExpectedConditions.presenceOf(fileUploader.sucessMark))
+        //browser.sleep(8000)
         fileUploader.uploadButton.click()
-        expect(fileUploader.dragAndDropId.getText()).toBe('File Uploaded!')
+    browser.sleep(8000)
+    //    expect(fileUploader.fileUploadedMessage.getText()).toBe('File Uploade!')
+    // console.log(fileUploader.dragAndDropId)
+
+
+
+
+
+
+
+
+
+    // browser.actions().
+    // dragAndDrop(fileUploader.dragAndDropId).
+    // perform();
+    // browser.wait(ExpectedConditions.presenceOf(fileUploader.dragAndDropId))
+    //     fileUploader.uploadButton.click()
+    //     expect(fileUploader.dragAndDropId.getText()).toBe('File Uploade!')
+
+        // browser.executeScript(dnd, fileUploader.dragAndDropId.getWebElement())
+        // fileUploader.dragAndDropId.getWebElement().then(function (element) {
+           
+        //     browser.wait(ExpectedConditions.presenceOf(element))
+        //     fileUploader.uploadButton.click()
+        //     expect(fileUploader.dragAndDropId.getText()).toBe('File Uploade!')
+        
+        //})
+        //dropFile(fileUploader.dragAndDropId, filePath);
+        // browser.executeScript(dnd, dragDrop.boxA.getWebElement(), dragDrop.boxB.getWebElement())
+        // browser.sleep(5000)
+        // // wait for the droped image to be displayed in the drop area
+        // browser.wait(ExpectedConditions.presenceOf(fileUploader.dragAndDropId))
+        // fileUploader.uploadButton.click()
+        // expect(fileUploader.dragAndDropId.getText()).toBe('File Uploaded!')
     })
 })
 
